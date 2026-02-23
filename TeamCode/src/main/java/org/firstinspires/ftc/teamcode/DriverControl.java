f19f5527d143f300b418652eb3ab834ecd6e4daa
anuthiru <149186696+anuthiru@users.noreply.github.com>
2026-02-23 18:24:15 -0500

    turret

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
@@ -4,16 +4,17 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Helper.DriveTrain;
-import org.firstinspires.ftc.teamcode.Helper.GamePad;
 import org.firstinspires.ftc.teamcode.Helper.Hardware;
 import org.firstinspires.ftc.teamcode.Helper.Shoot;
+import org.firstinspires.ftc.teamcode.Helper.Turret;
 
-@TeleOp(name = "Driver Control ", group = "Competition")
+@TeleOp(name = "Driver Control", group = "Competition")
 public class DriverControl extends LinearOpMode {
 
     private DriveTrain drive;
     private Shoot shooter;
-    private GamePad gp1, gp2;
+    private Turret turret;
+
     private boolean reversed = false;
     private double speedMultiplier = 0.9;
 
@@ -22,27 +23,20 @@ public class DriverControl extends LinearOpMode {
 
         Hardware.init(hardwareMap);
 
-        telemetry.addLine("Driver Control 2-Motor")
-                .addData("Version", "1.1");
-        telemetry.update();
-
         drive = new DriveTrain();
         shooter = new Shoot(Hardware.intakeMotor, Hardware.outtakeMotor);
+        turret = new Turret(Hardware.turretMotor);
 
-        // edge detection
         boolean lastBack = false;
         boolean lastA = false;
         boolean lastB = false;
 
         waitForStart();
-        telemetry.clear();
 
         while (opModeIsActive()) {
 
             boolean backPressed = gamepad1.back && !lastBack;
-            if (backPressed) {
-                reversed = !reversed;
-            }
+            if (backPressed) reversed = !reversed;
             lastBack = gamepad1.back;
 
             if (gamepad1.dpad_down)  speedMultiplier = 0.25;
@@ -57,34 +51,31 @@ public class DriverControl extends LinearOpMode {
                     reversed
             );
 
-
             boolean aPressed = gamepad2.a && !lastA;
             boolean bPressed = gamepad2.b && !lastB;
 
             if (aPressed) {
-                double intakePower = shooter.getLeftPower() > 0 ? 0 : 1;
-                shooter.setPower(intakePower, shooter.getRightPower());
+                double intakePower = shooter.getIntakePower() > 0 ? 0 : 1;
+                shooter.setPower(intakePower, shooter.getOuttakePower());
             }
 
             if (bPressed) {
-                double outtakePower = shooter.getRightPower() > 0 ? 0 : 0.6;
-                shooter.setPower(shooter.getLeftPower(), outtakePower);
+                double outtakePower = shooter.getOuttakePower() > 0 ? 0 : 0.6;
+                shooter.setPower(shooter.getIntakePower(), outtakePower);
             }
 
             lastA = gamepad2.a;
             lastB = gamepad2.b;
 
+            turret.update(
+                    gamepad2.right_stick_x,
+                    gamepad2.left_bumper,
+                    gamepad2.x,
+                    gamepad2.b,
+                    gamepad2.y
+            );
 
-            telemetry.addLine("Drive")
-                    .addData("Front Left", Hardware.frontLeft.getPower())
-                    .addData("Front Right", Hardware.frontRight.getPower())
-                    .addData("Back Left", Hardware.backLeft.getPower())
-                    .addData("Back Right", Hardware.backRight.getPower());
-
-            telemetry.addLine("Shooter/Intake")
-                    .addData("Intake Motor", Hardware.intakeMotor.getPower())
-                    .addData("Outtake Motor", Hardware.outtakeMotor.getPower());
-
+            telemetry.addData("Turret Position", turret.getPosition());
             telemetry.update();
         }
     }
