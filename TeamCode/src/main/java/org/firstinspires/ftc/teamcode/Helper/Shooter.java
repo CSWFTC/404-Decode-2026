3fbf6b7dd0e9b38c1e7f68bb9a0627884f617690
subissv <146556006+subissv@users.noreply.github.com>
2026-02-24 13:46:55 -0500

    code for outtake motor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/Shooter.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.Helper;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import android.os.SystemClock;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Helper.Hardware;
+
+import androidx.annotation.NonNull;
+
+//screws aren't in tight enough
+public class Shooter {
+
+    public static class Params{
+
+        public double pow = 0.9;
+
+
+    }
+    public static Shooter.Params PARAMS = new Shooter.Params();
+
+    public Shooter(@NonNull HardwareMap hdwMap){
+        Hardware.outtakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Hardware.outtakeMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Hardware.outtakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    public void motorPowerMax(){
+        Hardware.outtakeMotor.setPower(PARAMS.pow);
+    }
+
+    public void motorPowerZero(){
+        Hardware.outtakeMotor.setPower(0);
+    }
+}
